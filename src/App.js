fff100a6af1f0884c05790ebfcaed8e926f35448
rakshit <rakshit@gmail.com>
2020-04-22 17:21:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="input-data">
+                <h1>Calculator</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
